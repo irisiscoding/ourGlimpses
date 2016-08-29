46334b6ef209b00ee3011c5061191128a034ef4b
Yung Kim <gim.yoong@gmail.com>
2016-08-29 13:06:54 -0700

    added aws-sdk

--- server.js
+++ server.js
@@ -7,6 +7,37 @@ var bodyParser = require('body-parser');
 var http = require('http');
 var https = require("https");
 
+// AWS D3 
+var AWS = require('aws-sdk');
+/**
+ * Don't hard-code your credentials!
+ * Export the following environment variables instead:
+ *
+ * export AWS_ACCESS_KEY_ID='AKID'
+ * export AWS_SECRET_ACCESS_KEY='SECRET'
+ */
+
+// Set your region for future requests.
+AWS.config.region = 'us-west-2';
+
+// Create a bucket using bound parameters and put something in it.
+
+var s3bucket = new AWS.S3({params: {Bucket: 'myBucket'}});
+
+// IMPORTANT: Make sure to change the bucket name from "myBucket" above to something unique.
+
+s3bucket.createBucket(function() {
+  var params = {Key: 'myKey', Body: 'Hello!'};
+  s3bucket.upload(params, function(err, data) {
+    if (err) {
+      console.log("Error uploading data: ", err);
+    } else {
+      console.log("Successfully uploaded data to myBucket/myKey");
+    }
+  });
+});
+// End of AWS D3
+
 var session = require('express-session')
 // Integrate body-parser with our App
 app.use(bodyParser.urlencoded({ extended: true }));
