d5b76774d575b98042a3e719fd03b45e0792f7c0
Yung Kim <gim.yoong@gmail.com>
2016-08-31 02:10:12 -0700

    updated aws s3

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var https = require("https");
 
 // AWS D3 
 var AWS = require('aws-sdk');
-AWS.config.loadFromPath('./config.json');
+// AWS.config.loadFromPath('./config.json');
 
 var s3 = new AWS.S3();
 s3.listBuckets(function(err, data) {
